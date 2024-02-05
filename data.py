8d367ffb3e83d8d5098c4fe18f1c829860337998
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-02-05 13:59:25 -0500

    Update data.py

--- data.py
+++ data.py
@@ -4,4 +4,48 @@ def tip_calculator(tip_percentage):
     total = tip + sub_total
     print(f"Your tip is {tip} and your total is {total}")
 
-tip_calculator(15)
+def word_count():
+    sentence = str.split(input("Enter a sentence: "))
+    words = 0 
+    for word in sentence:
+        words += 1
+    print(words)
+    
+
+
+def odd_or_even():
+    number = int(input("Give me a number: "))
+    if number%2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+
+
+def tip_based_on_percentage():
+    bill = int(input("Enter a bill: "))
+    service = input("How good was the service(bad, okay, good, great): ")
+    if service == "bad":
+        print("Don't pay a tip lol")
+    elif service == "okay":
+        print(f"Your tip is ${bill * 0.15}.")
+    elif service == "good":
+        print(f"Your tip is ${bill * 0.2}.")
+    elif service == "great":
+        print(f"Your tip is ${bill * 0.25}.")
+    else:
+        print("You didn't enter an option.")
+
+
+def factor():
+    number = int(input("Enter a number: "))
+    x = 1
+    factor = []
+    while x <= number:
+        if number%x == 0:
+            factor.append(x)
+            x += 1
+        else:
+            x += 1
+    print(factor)
+
+factor()
